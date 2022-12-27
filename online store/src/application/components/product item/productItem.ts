ed5c9ed00d22600688bb9efddfa20a7ee7412989
kasssper4 <alexey2snake@gmail.com>
2022-12-27 18:47:17 +0300

    feat: add product info to prod card

--- online store/src/application/components/product item/productItem.ts
+++ online store/src/application/components/product item/productItem.ts
@@ -19,11 +19,22 @@ export class ProductItem {
                 prodItemWrap.className = 'item-wrap';
                 const href = `#${PageIds.ProductPage}/${i + 1}`;
                 prodItemWrap.href = href;
+                const prodDescription = document.createElement('div');
+                prodDescription.classList.add('prod-item-description');
                 prodItemWrap.append(productItem.title);
+                prodDescription.insertAdjacentHTML('beforeend', `<div>Category: ${productItem.category}</div>`);
+                prodDescription.insertAdjacentHTML('beforeend', `<div>Brand: ${productItem.brand}</div>`);
+                prodDescription.insertAdjacentHTML('beforeend', `<div>Price: $${String(productItem.price)}</div>`);
+                prodDescription.insertAdjacentHTML(
+                    'beforeend',
+                    `<div>Discount: ${String(productItem.discountPercentage)}</div>`
+                );
+                prodDescription.insertAdjacentHTML('beforeend', `<div>Rating: ${String(productItem.rating)}</div>`);
+                prodDescription.insertAdjacentHTML('beforeend', `<div>Stock: ${String(productItem.stock)}</div>`);
 
                 const productImage = document.createElement('img');
                 prodImageWrap.append(productImage);
-                prodItemWrap.append(prodImageWrap);
+                prodItemWrap.append(prodDescription, prodImageWrap);
                 productImage.src = productItem.images[0];
 
                 this.prodList.append(prodItemWrap);
