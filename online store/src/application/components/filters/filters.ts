8f3b77f5829d5905c6d59dc7ece6b8915014a82d
DianaSmertina <am.dianasmertina@gmail.com>
2023-01-03 12:50:53 +0400

    feat: add all filters

--- online store/src/application/components/filters/filters.ts
+++ online store/src/application/components/filters/filters.ts
@@ -1,5 +1,6 @@
 export class FiltersList {
     static filterSection = document.createElement('section');
+    query: string;
     static categotiesArr = [
         'smartphones',
         'laptops',
@@ -22,26 +23,65 @@ export class FiltersList {
         'motorcycle',
         'lighting',
     ];
-    query: string;
+    static brandsArr = [
+        'Apple',
+        'Samsung',
+        'OPPO',
+        'Huawei',
+        'Microsoft Surface',
+        'Infinix',
+        'HP Pavilion',
+        'Impression of Acqua Di Gio',
+        'Royal_Mirage',
+        'Fog Scent Xpressio',
+        'Al Munakh',
+        'Lord - Al-Rehab',
+        "L'Oreal Paris",
+        'Hemani Tea',
+        'Dermive',
+        'ROREC White Rice',
+        'Fair and Clear',
+        'Saaf and Khaas',
+        'Bake Parlor Big',
+        'Baking Food Items',
+        'fauji',
+        'Dry Rose',
+        'Boho Decor',
+        'Flying Wooden',
+        'LED Lights',
+        'luxury palace',
+        'Golden',
+    ];
 
     constructor(query: string) {
         this.query = query;
     }
 
-    render() {
-        const queryParamsArr = this.query
+    getQueryParams(criterion: string) {
+        const queryParams = this.query
             .slice(1)
             .split('&')
             .filter((el) => {
-                el.split('=')[0] === 'categories';
-                return el;
+                if (el.split('=')[0] === criterion) return el;
             })
             .map((el) => el.split('=')[1]);
+        return queryParams;
+    }
 
-        const formCategories = document.createElement('form');
-        formCategories.className = 'filter-form categories';
+    createCheckbox(criterion: string) {
+        const queryParamsArr = this.getQueryParams(criterion);
+        const checkboxBlock = document.createElement('div');
+        checkboxBlock.className = 'checkbox';
+        const header = document.createElement('h3');
+        header.innerText = criterion;
+        const formElement = document.createElement('form');
+        formElement.className = `filter-form ${criterion}`;
+        checkboxBlock.append(header, formElement);
 
-        FiltersList.categotiesArr.forEach((el) => {
+        let arr;
+        criterion === 'categories' ? (arr = FiltersList.categotiesArr) : (arr = FiltersList.brandsArr);
+
+        arr.forEach((el) => {
             const inputWrap = document.createElement('p');
             inputWrap.className = 'filter-checkbox';
             let checked = false;
@@ -60,13 +100,13 @@ export class FiltersList {
                     const url = new URL(window.location.href);
                     const params = new URLSearchParams(url.search);
                     if ((inputWrap.firstChild as HTMLInputElement).checked) {
-                        params.append('categories', (inputWrap.firstChild as HTMLInputElement).value);
+                        params.append(criterion, (inputWrap.firstChild as HTMLInputElement).value);
                         window.location.search = params.toString();
                     } else {
                         const newQuery = this.query
                             .slice(1)
                             .split('&')
-                            .filter((el) => el !== `categories=${(inputWrap.firstChild as HTMLInputElement).value}`);
+                            .filter((el) => el !== `${criterion}=${(inputWrap.firstChild as HTMLInputElement).value}`);
                         window.location.search = '?' + newQuery.join('&');
                     }
                 });
@@ -76,11 +116,94 @@ export class FiltersList {
                 (inputWrap.firstChild as HTMLInputElement).checked = true;
             }
 
-            formCategories.append(inputWrap);
+            formElement.append(inputWrap);
         });
 
+        return checkboxBlock;
+    }
+
+    createRange(criterion: string) {
+        const queryParamsArrFrom = this.getQueryParams(criterion + 'From');
+        const queryParamsArrTo = this.getQueryParams(criterion + 'To');
+        const rangeBlock = document.createElement('div');
+        rangeBlock.className = 'slider';
+        const header = document.createElement('h3');
+        header.innerText = criterion;
+        const rangeWrap = document.createElement('div');
+        rangeWrap.className = `filter-slider ${criterion}`;
+        rangeBlock.append(header, rangeWrap);
+
+        let fromValue = 0;
+        let toValue = 100;
+        if (queryParamsArrFrom.length > 0) {
+            fromValue = Number(queryParamsArrFrom[0]);
+        }
+        if (queryParamsArrTo.length > 0) {
+            toValue = Number(queryParamsArrTo[0]);
+        }
+
+        rangeWrap.innerHTML = `<input id="fromSlider" type="range" value="${fromValue}" min="0" max="100"/>\
+            <input id="toSlider" type="range" value="${toValue}" min="0" max="100"/>`;
+
+        const fromInput = rangeWrap.firstChild;
+        const toInput = rangeWrap.lastChild;
+
+        fromInput?.addEventListener('input', () => {
+            const url = new URL(window.location.href);
+            const params = new URLSearchParams(url.search);
+            if (
+                queryParamsArrTo.length === 0 ||
+                Number((fromInput as HTMLInputElement).value) < Number(queryParamsArrTo[0])
+            ) {
+                if (queryParamsArrFrom.length > 0) {
+                    params.delete(criterion + 'From');
+                }
+                params.append(criterion + 'From', (fromInput as HTMLInputElement).value);
+            } else {
+                if (queryParamsArrTo.length > 0) {
+                    params.delete(criterion + 'From');
+                    params.delete(criterion + 'To');
+                    params.append(criterion + 'From', queryParamsArrTo[0]);
+                    params.append(criterion + 'To', (fromInput as HTMLInputElement).value);
+                }
+            }
+            window.location.search = params.toString();
+        });
+        toInput?.addEventListener('input', () => {
+            const url = new URL(window.location.href);
+            const params = new URLSearchParams(url.search);
+            if (
+                queryParamsArrFrom.length === 0 ||
+                Number((toInput as HTMLInputElement).value) > Number(queryParamsArrFrom[0])
+            ) {
+                if (queryParamsArrTo.length > 0) {
+                    params.delete(criterion + 'To');
+                }
+                params.append(criterion + 'To', (toInput as HTMLInputElement).value);
+            } else {
+                if (queryParamsArrFrom.length > 0) {
+                    params.delete(criterion + 'From');
+                    params.delete(criterion + 'To');
+                    params.append(criterion + 'To', queryParamsArrFrom[0]);
+                    params.append(criterion + 'From', (toInput as HTMLInputElement).value);
+                }
+            }
+            window.location.search = params.toString();
+        });
+
+        return rangeBlock;
+    }
+
+    render() {
+        FiltersList.filterSection.className = 'filters-wrap';
+
         if (!FiltersList.filterSection.firstChild) {
-            FiltersList.filterSection.append(formCategories);
+            FiltersList.filterSection.append(
+                this.createCheckbox('categories'),
+                this.createCheckbox('brand'),
+                this.createRange('price'),
+                this.createRange('stock')
+            );
         }
 
         return FiltersList.filterSection;
